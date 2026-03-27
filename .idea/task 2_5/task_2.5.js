817d7cb29a98e1b11b3cb841629a858dd6188d2c
bn-skn <tiket124@gmail.com>
2026-03-27 15:50:58 +0300

    Задача 2.5. типы данных

---
+++ .idea/task 2_5/task_2.5.js
@@ -0,0 +1,13 @@
+let name = "Ilya";
+
+console.log( `hello ${1}` ); // hello 1
+
+console.log( `hello ${"name"}` ); // hello name
+
+console.log( `hello ${name}` ); // hello Ilya
+
+let gugol = 123456789012345678901234567890n;
+console.log( gugol );
+let isOdd = 10 % 3 == 0;
+console.log( isOdd );
+typeof 1234456789012345678901234567890n;
